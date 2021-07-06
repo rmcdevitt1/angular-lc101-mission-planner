7c6422e7221da744baea7100663202071dd13542
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-07-06 16:00:10 -0500

    beginning studio 3

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -24,6 +24,7 @@ export class CrewComponent implements OnInit {
     this.crew.push({name: memberName, firstMission: isFirst});
   }
 
+
   remove(member: object) {
     let index = this.crew.indexOf(member);
     this.crew.splice(index, 1);
